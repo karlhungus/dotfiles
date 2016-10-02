df989144946ad8d5b097f8c665ff9fb7491ddd2d
Izaak Alpert (karlhungus) <ialpert@gmail.com>
2016-10-02 01:05:18 -0400

    add prezto

---
+++ files/.zlogin
@@ -0,0 +1,23 @@
+#
+# Executes commands at login post-zshrc.
+#
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+# Execute code that does not affect the current session in the background.
+{
+  # Compile the completion dump to increase startup speed.
+  zcompdump="${ZDOTDIR:-$HOME}/.zcompdump"
+  if [[ -s "$zcompdump" && (! -s "${zcompdump}.zwc" || "$zcompdump" -nt "${zcompdump}.zwc") ]]; then
+    zcompile "$zcompdump"
+  fi
+} &!
+
+# Print a smac adage
+if [[ $OSTYPE == darwin* ]]; then
+  /usr/local/bin/fortune smac
+fi
+if [[ $OSTYPE == linux* ]]; then
+  /usr/games/fortune smac
+fi
--- lib/deltas/source-repo.sh
+++ lib/deltas/source-repo.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 define_option short=r long=repo variable=repo desc="The repository (owner/repo) to clone"
 define_option short=s long=sha variable=sha desc="The SHA to check out"
+define_option short=R long=grecursive variable=grecursive desc="Clone recursive" default="true" value="true"
 define_option short=c long=cmd variable=cmd default="" desc="An optional command to run after cloning"
 
 valid_options() {
@@ -10,6 +11,7 @@ valid_options() {
 
   target_dir="/usr/local/src/${repo##*/}"
   cmd="${cmd:-}"
+  grecursive="${grecursive}"
 }
 
 applied() {
@@ -25,7 +27,14 @@ applied() {
 apply() {
   if [ ! -d "${target_dir}" ]; then
     mkdir -p "${target_dir}"
-    git clone "https://github.com/${repo}" "${target_dir}"
+    if [ $grecursive == "true" ]
+    then
+      echo "recursive ${repo} ${target_dir}"
+      git clone --recursive "https://github.com/${repo}" "${target_dir}"
+    else
+      echo "nonrecursive ${repo} ${target_dir}"
+      git clone "https://github.com/${repo}" "${target_dir}"
+    fi
   fi
 
   pushd "${target_dir}" >/dev/null
