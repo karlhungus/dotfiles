0db1bd5e593b5c7c69c60434b114f78118c6a335
Izaak Alpert (karlhungus) <ialpert@gmail.com>
2017-06-22 13:46:58 -0700

    update gpg key

--- lib/phases.sh
+++ lib/phases.sh
@@ -70,7 +70,7 @@ __install_development_environments() {
 }
 
 __install_source_dependencies() {
-  apply_delta "fetch oh-my-zsh" "bin/apply source-repo -r robbyrussell/oh-my-zsh --sha b908feeb" || return $?
+  apply_delta "fetch oh-my-zsh" "bin/apply source-repo -r robbyrussell/oh-my-zsh --sha b908fee" || return $?
   apply_delta "plug vim" "bin/apply vim-plug" || return $?
   apply_delta "install powerline fonts" "bin/apply source-repo --repo powerline/fonts --sha a83a867 --cmd ./install.sh" || return $?
 }
