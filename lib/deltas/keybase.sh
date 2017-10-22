c3b70a5751e82e5a9f4cfb3511381bcb6128f122
Izaak Alpert (karlhungus) <ialpert@gmail.com>
2017-10-22 15:51:36 -0400

    let keybase run despite the errors

--- lib/deltas/keybase.sh
+++ lib/deltas/keybase.sh
@@ -11,10 +11,10 @@ apply() {
   elif linux; then
     download_file https://prerelease.keybase.io/keybase_amd64.deb /tmp
     # if you see an error about missing `libappindicator1`
-    # # from the next command, you can ignore it, as the
-    # # subsequent command corrects it
-    sudo dpkg -i /tmp/keybase_amd64.deb
-    sudo apt-get install -f
+    # from the next command, you can ignore it, as the
+    # subsequent command corrects it
+    sudo dpkg -i --force-all /tmp/keybase_amd64.deb
+    sudo apt-get install -f -y
     run_keybase
     rm -rf /tmp/keybase_amd64.deb
   fi
