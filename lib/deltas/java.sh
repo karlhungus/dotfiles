f94bd492f703a9fa2785a9a48ef573fc5fbec634
Izaak Alpert (karlhungus) <ialpert@gmail.com>
2017-10-24 12:05:55 -0400

    sbt is dependent on java

--- lib/deltas/java.sh
+++ lib/deltas/java.sh
@@ -2,14 +2,13 @@
 set -euo pipefail
 
 applied() {
-  # you need java to install java... fuuuuu oracle
-  if osx; then return 0; fi
   [[ $(which java && java -version) =~ "java" ]]
 }
 
 apply() {
   if osx; then
     brew cask install java
+    apply_delta "install sbt" "bin/apply packages sbt" || return $?
   elif linux; then
     sudo apt-get install -y default-jre
   fi
