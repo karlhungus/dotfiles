af72fa8f060c465f798ca8ba06cca0bc22edda96
Izaak Alpert (karlhungus) <ialpert@gmail.com>
2017-10-24 12:50:47 -0400

    osx fortune doesnt know about version

--- lib/deltas/fortune-mod-smac.sh
+++ lib/deltas/fortune-mod-smac.sh
@@ -3,6 +3,11 @@ set -euo pipefail
 
 applied() {
   PATH=$PATH:/usr/games
+  if osx; then
+    if [["$(which fortune && fortune smac &&fortune -version)" =~ "fortune-mod version" ]]; then
+      return 0
+    fi
+  fi
   [[ "$(which fortune && fortune smac -version)" =~ "fortune-mod version" ]]
 }
 
