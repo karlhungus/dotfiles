32552e63a6091a3b88290e2b77fa24dd3cf22382
Izaak Alpert (karlhungus) <ialpert@gmail.com>
2017-10-30 12:12:31 -0400

    update php

--- lib/deltas/php.sh
+++ lib/deltas/php.sh
@@ -9,8 +9,10 @@ apply() {
   if osx; then
     brew tap homebrew/homebrew-php
     brew update
-    brew install php56 --with-pear
+    brew install php70 --with-pear
+    sudo pear channel-discover pear.symfony-project.com
   elif linux; then
-    sudo apt-get install -y apache2 php mysql-server php-mysql
+    sudo apt-get install -y apache2 php mysql-server php-mysql php-pear
+    sudo pear channel-discover pear.symfony-project.com
   fi
 }
