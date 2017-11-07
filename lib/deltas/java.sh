64596c51a6d7cdc501f0b1bf41b498a87a143c69
Izaak Alpert (karlhungus) <ialpert@gmail.com>
2017-11-06 20:38:04 -0500

    sbd doesn't work with java 9

--- lib/deltas/java.sh
+++ lib/deltas/java.sh
@@ -9,7 +9,6 @@ applied() {
 apply() {
   if osx; then
     brew cask install java
-    apply_delta "install sbt" "bin/apply packages sbt" || return $?
   elif linux; then
     sudo apt-get install -y default-jre
   fi
